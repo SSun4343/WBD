3f7a890c6e88f1df5c44d12d553d721d8e0e99d7
Sunsuny <sunsun4343@naver.com>
2025-04-21 14:33:36 +0900

    Init

---
+++ convert.py
@@ -0,0 +1,8 @@
+import csv, json
+
+with open("data.csv", newline='', encoding="utf-8") as csvfile:
+    reader = csv.DictReader(csvfile)
+    rows = list(reader)
+
+with open("data.json", "w", encoding="utf-8") as jsonfile:
+    json.dump(rows, jsonfile, indent=2, ensure_ascii=False)
